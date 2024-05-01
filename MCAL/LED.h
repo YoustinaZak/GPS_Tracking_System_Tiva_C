0221936682455057b0eb5a1cb2b9b5fedd84ba67
Mark Magdy <111954245+mark-magdy@users.noreply.github.com>
2024-05-02 01:17:09 +0300

    Update LED.h

--- MCAL/LED.h
+++ MCAL/LED.h
@@ -1,1 +1,15 @@
+#ifndef LED_H_
+#define LED_H_
 
+#include "../LIB/tm4c123gh6pm.h"
+#include "../LIB/STD_TYPES.h"
+#include "../LIB/BIT_MATH.h"
+
+#define RED 0x2
+#define BLUE 0x4
+
+void TURN_ON_LED(uint8_t Colour);
+void Check_Distance(float distance, uint8_t Button_State);
+
+
+#endif
