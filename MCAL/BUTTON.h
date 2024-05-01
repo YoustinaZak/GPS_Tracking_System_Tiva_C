93247a0c4ba1fc88af3c7e7cfed17d45d6f3e9c3
michealshohda <112519102+michealshohda@users.noreply.github.com>
2024-05-01 23:18:47 +0300

    Update BUTTON.h

--- MCAL/BUTTON.h
+++ MCAL/BUTTON.h
@@ -1,1 +1,12 @@
+#ifndef BUTTON_H_
+#define BUTTON_H_
 
+#include "GPIO.h"
+#include "../LIB/STD_TYPES.h"
+
+#define BUTTON_PRESSED  0x0
+#define BUTTON_NOT_PRESSED 0x1
+
+uint8_t Button_Status();
+
+#endif
