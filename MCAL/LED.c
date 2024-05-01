ee057f4b6dbde7d816c8ff78b5e2646bd73cd41c
jessyfady <160116268+jessyfady@users.noreply.github.com>
2024-05-01 23:28:20 +0300

    Update LED.c

--- MCAL/LED.c
+++ MCAL/LED.c
@@ -1,1 +1,21 @@
+#include "LED.h"
 
+void TURN_ON_LED(uint8_t Colour)
+	{
+		CLEAR_BIT(GPIO_PORTF_DATA_R,1);
+		CLEAR_BIT(GPIO_PORTF_DATA_R,2);
+		CLEAR_BIT(GPIO_PORTF_DATA_R,3);
+		GPIO_PORTF_DATA_R |= Colour;
+  }
+	
+void Check_Distance(float distance, uint8_t Button_State)
+	{
+		if (distance >=100 || Button_State == 1)
+			{
+			   TURN_ON_LED(GREEN);
+		  }
+		else 
+			{
+					TURN_ON_LED(RED);
+			}
+  }
