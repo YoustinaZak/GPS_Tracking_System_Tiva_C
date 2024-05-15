2334d482436442c67d282056d0068b2b2da78895
Fadyrefat <167587049+Fadyrefat@users.noreply.github.com>
2024-05-16 00:52:57 +0300

    Update GPS.h

--- HAL/GPS.h
+++ HAL/GPS.h
@@ -1,1 +1,22 @@
+#ifndef GPS_H_
+#define GPS_H_
 
+
+#include "STD_TYPES.h"
+#define MAX_FIELDS 14
+#define MAX_FIELD_LENGTH 20
+#define NO_OF_COORDINATES 20  //should be 200
+#define PI 3.14159265358979323846 // Standard value of pi
+
+
+extern float Extracted_Coordinates[1000][2];
+
+float toRad(float num);
+void Get_Data(uint8_t data[]);
+uint8_t Check_fix();
+
+void Process_str(uint8_t input[], double * latitude, double * longitude);
+void Get_Map_Coordinates(double latitude, double longitude, uint8_t i);
+float Get_GPS_distance(float currentlong, float currentlat, float deslong, float deslat);
+
+#endif
