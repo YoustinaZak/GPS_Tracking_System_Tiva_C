2e826ef11cdeb413aa98557d29d01657c14fff68
michealshohda <112519102+michealshohda@users.noreply.github.com>
2024-05-16 00:03:12 +0300

    Update GPS.c

--- HAL/GPS.c
+++ HAL/GPS.c
@@ -1,1 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+#include "GPS.h"
+#include "GPIO.h"
+#include "UART.h"
+#include "STD_TYPES.h"
+
+	
+void Get_Data(uint8_t data[])
+	{
+		uint8_t i;
+		while((UART2_FR_R & (1<<4))!=0){} //receive flag
+		if( UART2_DR_R =='$')
+				  {
+						for(i=0; i<50; i++)
+						{
+					    data[i]= UART2_DR_R;
+							if(data[i]=='*')
+								{
+									break;
+								}
+							while((UART2_FR_R & (1<<4))!=0){}
+				    }		 
+	        }
+					
+	}
+	
+	
+uint8_t Check_fix (uint8_t inputChar)
+{
+		if(inputChar =='1')
+			{
+				return 1;
+			}
+		else
+			{
+				return 0;
+			}
+}
+
+
+void Process_str(uint8_t input[], double * latitude, double * longitude) 
+	{
+		
+		char *token;
+		int fieldCount = 0;
+		
+		// divide input string by comma
+    token = strtok(input, ",");
+		
+    while (token != NULL && fieldCount < MAX_FIELDS) ////////////check the token condtion seems wired
+			{
+				
+				if(fieldCount==2)
+					{
+						*latitude = atof(token);
+						 
+				  }
+				else if(fieldCount==4)
+					{
+						 *longitude = atof(token);
+				  }
+					else if(fieldCount==6)
+					{
+						if(!Check_fix(*token))     //check that the data is valid
+						{
+							*latitude=0;
+							*longitude =0;
+						}
+						break;
+					} 
+        fieldCount++;
+        token = strtok(NULL, ",");    //get another string starting from Null to ,
+      }		
+}
+
+
+void Get_Map_Coordinates(double latitude, double longitude, uint8_t i)
+	{
+     uint8_t lat_deg=0;
+     float lat_min=0;
+		
+     float lon_deg=0;
+     float lon_min=0;
+
+    //calc latitude
+    lat_deg = (uint8_t)(latitude / 100);
+    lat_min = latitude - lat_deg * 100;
+    //latitude = lat_deg + lat_min / 60;
+    Extracted_Coordinates[i][0]= lat_deg + lat_min / 60;
+
+    //calc longitude
+     lon_deg = (uint8_t)(longitude / 100);
+     lon_min = longitude - lon_deg * 100;
+     //longitude = lon_deg + lon_min / 60;
+		 Extracted_Coordinates[i][1]= lon_deg + lon_min / 60;
+				
+}
