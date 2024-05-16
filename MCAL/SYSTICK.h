70a39b1164f1137498618fcf0ef63450353da7ad
jessyfady <160116268+jessyfady@users.noreply.github.com>
2024-05-16 19:25:32 +0300

    Update SYSTICK.h

--- MCAL/SYSTICK.h
+++ MCAL/SYSTICK.h
@@ -1,1 +1,15 @@
+#ifndef SYSTICK_H_
+ #define SYSTICK_H_
+ 
+ #include "tm4c123gh6pm.h"
+ 
+ void SYSTICK_INIT();
+ void delay_a_sec();
+ void delay_in_seconds(int sec);
+ void delay_a_millis();
+ void delay_in_millis(int millisec);
+ void delay_a_micros();
+ void delay_in_micros(int microsec);
 
+ 
+ #endif
